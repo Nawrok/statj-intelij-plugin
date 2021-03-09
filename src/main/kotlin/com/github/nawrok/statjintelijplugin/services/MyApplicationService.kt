52f08922579f3b2458a7928e8436cf6ee661bfcb
GitHub Action <action@github.com>
2021-03-09 08:59:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/nawrok/statjintelijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nawrok/statjintelijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nawrok.statjintelijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nawrok.statjintelijplugin.MyBundle
 
 class MyApplicationService {
 
