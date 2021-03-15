93dde076a6cef85e148113fee509f5bf9172a3a8
Bartlomiej Malkowski <224370@edu.p.lodz.pl>
2021-03-15 19:49:08 +0100

    move text to AppBundle.properties add information to statisticToString method

--- src/main/java/pl/lodz/zzpj/statj/actions/StatisticAction.java
+++ src/main/java/pl/lodz/zzpj/statj/actions/StatisticAction.java
@@ -5,27 +5,29 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
+import pl.lodz.zzpj.statj.AppBundle;
 import pl.lodz.zzpj.statj.model.Statistic;
 import pl.lodz.zzpj.statj.service.StatisticService;
 
 import java.util.Objects;
 
-public class StatisticAction extends AnAction
-{
+public class StatisticAction extends AnAction {
     private final StatisticService service = new StatisticService();
 
     @Override
-    public void actionPerformed(@NotNull AnActionEvent e)
-    {
+    public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = Objects.requireNonNull(e.getProject());
         service.setProject(project);
 
         String stats = statisticToString(service.getStatistic());
-        Messages.showMessageDialog(project, stats, "Statistics", null);
+        Messages.showMessageDialog(project, stats, AppBundle.INSTANCE.getMessage("statistics"), null);
     }
 
-    private String statisticToString(Statistic statistic)
-    {
-        return "Methods: " + statistic.getMethods();
+    private String statisticToString(Statistic statistic) {
+        String colon = ": ";
+        char lineBreaker = '\n';
+        return AppBundle.INSTANCE.getMessage("classes") + colon + statistic.getClasses() + lineBreaker +
+                AppBundle.INSTANCE.getMessage("methods") + colon + statistic.getMethods() + lineBreaker +
+                AppBundle.INSTANCE.getMessage("fields") + colon + statistic.getFields();
     }
 }
